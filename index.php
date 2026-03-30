e0d9a642355bf842199d220585dbe77b359ddfb2
BramantyoApk <zerocraft087@gmail.com>
2026-03-30 18:42:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TuMal</title>
+</head>
+<body>
+    <div class="hero">
+        <p>Halo semua, perkenalkan nama ku <strong>Bramantyo Kusuma</strong></p>
+    </div>
+    <div class="navbar">
+        <a href="about/">About</a>
+        <a href="karya/">Karya</a>
+        <a href="contact/">Contact</a>
+    </div>
+
+
+
+</body>
+</html>
